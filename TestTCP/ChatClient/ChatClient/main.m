c3e3522ef7c65ff9e910f4c7d1109fc3a70391e0
Guilherme Andrade <gbuenoandrade@gmail.com>
2015-05-05 00:22:19 -0300

    server script

---
+++ TestTCP/ChatClient/ChatClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChatClient
+//
+//  Created by Guilherme Andrade on 5/5/15.
+//  Copyright (c) 2015 Unicamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
